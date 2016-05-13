964c6b911d2196d9adc202a7a01600f6cdcdd5d1
Evan Raskob <info@pixelist.info>
2016-05-13 14:49:54 +0100

    added image loading and some css styling and popups

--- nick/Mapping/main.js
+++ nick/Mapping/main.js
@@ -1,6 +1,8 @@
 console.log(L);
 
-var mymap = L.map('mapid').setView([51.505, -0.09], 8);
+var mymap = L.map('mapid');
+
+mymap.setView([61.505, -0.09], 8);
 
 console.log(mymap);
 
@@ -11,6 +13,8 @@ L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v8/tiles/{z}/{x}/{y}?a
     accessToken: 'pk.eyJ1IjoiY2Fzc2llbGRvdGNvbSIsImEiOiJfOXlpQmFJIn0.W5qn7J_RcOV8502RXsDc3Q'
 }).addTo(mymap);
 
+
+
 //
 // made this a function to show how that works
 //
@@ -19,33 +23,54 @@ function jsonFunc(data, success) {
     console.log(success);
     
     for (var i = 0; i < data.length; i++) {
-        console.log(data[i].position);
         var pos = data[i].position;
-        L.marker(pos).addTo(mymap);
+        
+        L.marker(pos).addTo(mymap).bindPopup(
+            '<div class="popup">'+data[i].name+'</div>' +
+            '<div class="popup"><img src="' + data[i].img + '"/></div>'
+        );
     }
+    
+    mymap.setView(data[data.length-1].position, 8);
 }
 
 //
 // added error handling
 //
-$.getJSON("data2.json", jsonFunc)
-  .done(function() {
-    console.log( "second success" );
-  }).fail(function (jqXHR, textStatus, error) {
-    console.log("Post error: " + error);
-    alert("Failed: " + error);
-  }).always(function() {
-    // runs after this is complete, always, no matter what
-    console.log( "complete" );
-  });
+//$.getJSON("data.json", jsonFunc)
+//  .done(function() {
+//    console.log( "second success" );
+//  }).fail(function (jqXHR, textStatus, error) {
+//    console.log("Post error: " + error);
+//    alert("Failed: " + error);
+//  }).always(function() {
+//    // runs after this is complete, always, no matter what
+//    console.log( "complete" );
+//  });
 
 
 // the json data is just the following, we could have copied/pasted it:
 //
-//var myData = [
-//    {"name": "someone", "position": [50.0, 0.0]},
-//    {"name": "someone else", "position": [50.0, 0.1]},
-//    {"name": "Margate", "position": [51.3770212, 1.3480567]}
-//];
-//
-//jsonFunc(myData);
+var myData = [
+    //{"name": "someone", "position": [50.0, 0.0]},
+    {
+        "name": "testdata", 
+        "position": ["51,30.1677N", "0,0.7141E"],
+        "img": 'http://cuteimages.net/data/2015/11/small-kitten-photos-cuteimages.net.jpg'
+    },
+    {
+        "name": "Margate", 
+        "position": [51.3770212, 1.3480567], 
+        "img": 'https://media.giphy.com/media/NcK0Arww9YWg8/giphy.gif'
+    }
+];
+
+jsonFunc(myData);
+
+
+
+
+
+//L.marker(blackwalltunnelLoc, {icon: pinIcon}).addTo(map)
+//			.bindPopup('Blackwall Tunnel');
+//			//.openPopup();
